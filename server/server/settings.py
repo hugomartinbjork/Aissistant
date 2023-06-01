cc507d8a6180bb6045e64a928d3f8bea799a01bf
Hugo Bjork <hugobjork@me.com>
2023-06-01 19:12:28 +0200

    deploy

--- server/server/settings.py
+++ server/server/settings.py
@@ -27,11 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
-
+CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 
 INSTALLED_APPS = [
